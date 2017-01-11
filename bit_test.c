20228e404cc24735915d929d002b1ac520d1a9c5
西本拓海 <i13nisimoto@iebsd17.ie.tokuyama.ac.jp>
2017-01-11 15:53:40 +0900

    無駄な処理の削除

--- bit_test.c
+++ bit_test.c
@@ -1,37 +1,38 @@
 #include <stdio.h>
+#define HEIGHT 8
+#define WIDTH 8
 
-unsigned char getPoint(unsigned short bit_y,unsigned char x){
+unsigned short bitmap[HEIGHT]={};
+void init(){
+
+}
+unsigned char getPoint(unsigned char x,unsigned char y){
   unsigned short tmp;
   const unsigned short mask=3;
-  return tmp=(bit_y>>((7-x)*2))&mask;
+  return tmp=(bitmap[y]>>((7-x)*2))&mask;
 }
 
-void printMap(unsigned short c[]){
-  for(int i=0;i<8;i++){
-    for(int j=0;j<8;j++){
-      printf("%d",getPoint(c[i],j));
+void printMap(){
+  for(int i=0;i<HEIGHT;i++){
+    for(int j=0;j<WIDTH;j++){
+      printf("%d",getPoint(j,i));
     }
     printf("\n");
   }
 }
-void setPoint(unsigned short *bit,unsigned char x,unsigned char y,unsigned char color){
+void setPoint(unsigned char x,unsigned char y,unsigned char color){
   unsigned short mask=3;
   mask=~(mask<<((7-x)*2));
-  bit[y]&=mask;
+  bitmap[y]&=mask;
   mask=(short)color;
   mask=(mask<<((7-x)*2));
-  bit[y]=bit[y]|mask;
+  bitmap[y]=bitmap[y]|mask;
 }
 
+void debug(){
 
+}
 int main(){
-  unsigned short bitmap[8];
-  //bitmap[0]=255*2;
-  //bitmap[1]=50;
-
-  for(int i=2;i<8;i++){
-    bitmap[i]=0;
-  }
-  setPoint(bitmap,0,1,3);
-  printMap(bitmap);
+  init();
+  debug();
 }
